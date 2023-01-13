c3488ceae648bab0d7896173742bcd84cad4c76e
Oussama Souayri <souayri.oussama@gmail.com>
2023-01-13 10:49:20 +0100

    some updates in app componenet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'YouBooking';
+  authPage:boolean=false;
+  errorPage:boolean=false;
+  ngOnInit(): void {
+    if(window.location.href.includes("login") || window.location.href.includes("register") ){
+      this.authPage= true;
+    }
+    if(window.location.href.includes("404") || window.location.href.includes("403") ){
+      this.errorPage= true;
+    }
+  }
 }
